70326bb8c7d0da2693c301398e858f2dbd433054
urvashi2410 <urvashianand0906@gmail.com>
2022-04-30 21:39:10 +0530

    signup form

--- blogProject/accounts/views.py
+++ blogProject/accounts/views.py
@@ -1,10 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def signup_view(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            return redirect('article_page')
+    else:
+        form = UserCreationForm()
     data = {
         'form' : form
     }
-    return render(request, 'signup.html', data )
+    return render(request, 'signup.html', data)
+
--- blogProject/article/urls.py
+++ blogProject/article/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('', views.article),
+    path('', views.article, name='article_page'),
     path('comment/', views.comment),
     path('<slug:slug>/', views.article_details),
 ]
