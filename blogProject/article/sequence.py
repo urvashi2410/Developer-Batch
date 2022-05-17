a429327d3cc3553254d830ba19a442fa9848c959
urvashi2410 <urvashianand0906@gmail.com>
2022-05-17 21:08:01 +0530

    project complete

---
+++ blogProject/article/sequence.py
@@ -0,0 +1,12 @@
+# createartcle.html page url.py path add views.py normal fxn render 
+# loginrequired 
+# login.html next if check 
+# views.py url print krke dekhna  
+# url = request.POST.get('next')
+# form.py 
+# views.py createarticle fxn usme le lena form ko 
+# createarticle.html me show kr dena 
+# save krna views.py instance 
+# models user bnana foreignkey 
+# makemigrations run 
+# migrate run 
--- blogProject/article/views.py
+++ blogProject/article/views.py
@@ -1,16 +1,12 @@
 from django.shortcuts import redirect, render
 from article.form import CreateArticle
 from .models import Article 
-from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def article(request):
     articles = Article.objects.all()
-    data = {
-        'articles' : articles 
-    }
-    return render(request, 'article.html', data)
+    return render(request, 'article.html', {'articles' : articles})
 
 def article_details(request, slug):
     articles = Article.objects.get(slug = slug)
@@ -28,8 +24,9 @@ def create_article(request):
     if request.method == 'POST':
         data = CreateArticle(request.POST, request.FILES)
         if data.is_valid():
-            instance = data.save(commit = False)
+            instance = data.save(commit=False)
             instance.user = request.user
-            return redirect("article_page")
+            instance.save()
+            return redirect('article_page')
     form = CreateArticle()
-    return render(request, 'create_article.html', {'form': form})
+    return render(request, 'create_article.html', {'form' : form})
