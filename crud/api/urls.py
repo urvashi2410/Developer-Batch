803b7c5568878437f12b350466fb4e878b06ea9d
urvashi2410 <urvashianand0906@gmail.com>
2022-06-11 21:03:33 +0530

    class based view crud

--- crud/api/urls.py
+++ crud/api/urls.py
@@ -2,7 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('todo/', views.TodoClassBasedView().as_view()),
+    path('todo/<int:pk>', views.TodoClassBasedView().as_view()),
+]
+
+'''
     path('read/', views.read_view),
     path('read/<int:pk>', views.read_view_particular),
-    path('create/', views.create_view)
-]
+    path('create/', views.create_view),
+    path('update/<int:pk>', views.update_view),
+    path('delete/<int:pk>', views.delete_view)
+    '''
--- crud/api/views.py
+++ crud/api/views.py
@@ -1,10 +1,14 @@
+from urllib import response
 from api.serialiser import TodoSerialiser
 from .models import Todo 
 from .serialiser import TodoSerialiser
 from rest_framework.response import Response 
 from rest_framework.decorators import api_view
+from rest_framework.views import APIView
+from rest_framework import status
 # Create your views here.
 
+'''
 @api_view(['GET'])
 def read_view(request):
     todos = Todo.objects.all()
@@ -27,4 +31,62 @@ def create_view(request):
         s.save()
     return Response(s.data)
 
+@api_view(['POST'])
+def update_view(request, pk):
+    try:
+        task = Todo.objects.get(id = pk)
+    except:
+        return Response('No data found')
+    s = TodoSerialiser(instance=task, data=request.data)
+    if s.is_valid():
+        s.save()
+    return Response(s.data)
+
+@api_view(['DELETE'])
+def delete_view(request, pk):
+    try:
+        task = Todo.objects.get(id = pk)
+    except:
+        return Response('No data found')
+    task.delete()
+    return Response('Item deleted')
+'''
+
+class TodoClassBasedView(APIView):
+    def get(self, request, pk=None, format=None):
+        if pk is not None:
+            try:
+                todo = Todo.objects.get(id = pk)
+            except:
+                return Response('No data found', status=status.HTTP_404_NOT_FOUND)
+            s = TodoSerialiser(todo)
+            return Response(s.data, status=status.HTTP_200_OK)
+        todo = Todo.objects.all()
+        s = TodoSerialiser(todo, many=True)
+        return Response(s.data, status=status.HTTP_200_OK)
+
+    def post(self, request, pk=None, format=None):
+        s = TodoSerialiser(data=request.data)
+        if s.is_valid():
+            s.save()
+            return Response(s.data, status=status.HTTP_201_CREATED)
+        return Response(s.errors, status=status.HTTP_406_NOT_ACCEPTABLE)
     
+    def put(self, request, pk=None, format=None):
+        try:
+            task = Todo.objects.get(id = pk)
+        except:
+            return Response('No data found')
+        s = TodoSerialiser(task, data=request.data)
+        if s.is_valid():
+            s.save()
+            return Response(s.data, status=status.HTTP_201_CREATED)
+        return Response(s.errors, status=status.HTTP_406_NOT_ACCEPTABLE)
+
+    def delete(self, request, pk=None, format=None):
+        try:
+            task = Todo.objects.get(id = pk)
+        except:
+            return Response('No data found', status=status.HTTP_404_NOT_FOUND)
+        task.delete()
+        return Response('Deleted!', status=status.HTTP_200_OK)
